bfd82b24d0bb82007e2c41e4a9d603c9b85d60e8
DSlundro <dragan.savic95@outlook.it>
2022-04-05 12:40:12 +0200

    random numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,11 +53,26 @@ function startGame() {
     }
 }
 
+// In seguito l'utente clicca su una cella:
+// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba
+// la cella si colora di rosso e la partita termina,
+// altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando:
+// il giocatore clicca su una bomba
+// o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, 
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
-
-
-
-
-
+// genero 16 numeri random
+// creo un array per conteneri i numeri ottenuti
+let randomNumbers = [];
+// inizializzo un ciclo
+while(randomNumbers.length < 16){
+    // creo i numeri random
+    let number = Math.floor(Math.random() * 16) + 1;
+    // pusho nel array i numeri unici ottenuti
+    if(randomNumbers.indexOf(number) === -1) randomNumbers.push(number);
+}
+//console.log(randomNumbers);
 
